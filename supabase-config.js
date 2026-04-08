eb35c8f14b68b36cd04fe012044b1d031feb5cae
parkerbaines <parker.baines@cjfranke.com>
2026-04-08 11:16:44 -0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,1 @@
-window.FLEET_HUB_SUPABASE_CONFIG = {
-  url: "https://zmxynphetgelskqnadbg.supabase.co",
-  anonKey: "sb_publishable_0_oomNmeT4nT9AxDlxCUGQ_dQM8f_q_",
-};
+https://zmxynphetgelskqnadbg.supabase.co/functions/v1/fleetops-app
